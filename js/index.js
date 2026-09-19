728ede1466083eb7fd93d2240cf7a453afe430d7
n-456 <giadinhngoccantho@gmail.com>
2026-09-19 14:04:05 +0700

    tách css, js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+
+    function w3_open() {
+      document.getElementById("mySidebar").style.display = "block";
+    }
+
+    function w3_close() {
+      document.getElementById("mySidebar").style.display = "none";
+    }
+    
 loadProfile();
 
 document.getElementById("sign_out").addEventListener(
