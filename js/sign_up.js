0ba8046aea1f8a680c487f19be6ee923f5f53b15
n-456 <giadinhngoccantho@gmail.com>
2026-09-26 15:29:39 +0700

    validate form

--- js/sign_up.js
+++ js/sign_up.js
@@ -1,10 +1,31 @@
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      else {
+        event.preventDefault();
+        submitForm();
+      }
+
+    }, false)
+  })
+})()
+
+
+
 urlBE = "https://booking-management-backend-kdwt.onrender.com"
 
-document.getElementById("sign_up").addEventListener(
-    "click",
-    e => {
-        e.preventDefault();
-
+   function submitForm() {
         const customerData = {
             name: document.getElementById("name").value,
             phone: document.getElementById("phone").value,
@@ -14,7 +35,6 @@ document.getElementById("sign_up").addEventListener(
 
         addCustomer(customerData);
     }
-)
 
 async function addCustomer(customerData) {
     try {
@@ -34,8 +54,8 @@ async function addCustomer(customerData) {
         console.log("Thêm thành công: ", newCustomer);
         document.querySelector("form").reset();
 
-        alert("Đăng ký thành công. Hãy đăng nhập để tiếp tục")
-        window.location.href = "http://localhost:5500/component/sign_in.html";
+        // alert("Đăng ký thành công. Hãy đăng nhập để tiếp tục")
+        window.location.href = "http://localhost:5500/index.html";
     } catch (error) {
         console.error("Lỗi: ",error);
     }
